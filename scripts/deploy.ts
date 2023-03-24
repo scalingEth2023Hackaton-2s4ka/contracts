e8f30bd0c5b9094e3c6b9b09929328b60df6d8ed
Maxi Nivoli <m_nivoli@hotmail.com>
2023-03-24 14:38:13 -0300

    feat: Add update ownership

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,16 @@
 import { ethers } from 'hardhat';
 
 async function main() {
+  const linkToken = '0x779877A7B0D9E8603169DdbD7836e478b4624789';
+  const operator = '0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD';
+  const jobId = ethers.utils.toUtf8Bytes('c1c5e92880894eb6b27d3cae19670aa3');
   const XscrowFactory = await ethers.getContractFactory('XscrowFactory');
+
   console.log('before deploy');
   const xscrowFactory = await XscrowFactory.deploy(
-    '0x326C977E6efc84E512bB9C30f76E30c160eD06FB',
-    '0xCC79157eb46F5624204f47AB42b3906cAA40eaB7',
-    ethers.utils.toUtf8Bytes('c1c5e92880894eb6b27d3cae19670aa3')
+    linkToken,
+    operator,
+    jobId
   );
   console.log('after deploy', xscrowFactory);
   const contract = await xscrowFactory.deployed();
